647a7aa73f6a12c075f52aea4dbd6858eaadf83a
Murfidar <murfidar@gmail.com>
2023-10-08 21:10:54 +0700

    feat: add server using express

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import route from './routes';
+
+const app = express();
+const port = 3000;
+
+app.use(route);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
