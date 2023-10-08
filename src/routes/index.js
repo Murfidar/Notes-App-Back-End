43f6e667e1380b0f514e53748756ad57159f234f
Murfidar <murfidar@gmail.com>
2023-10-08 21:18:23 +0700

    feat: add route

---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import Controller from '../controller';
+
+const router = Router();
+
+router
+  .get('/notes', Controller.getAllNotes)
+  .post('/notes', Controller.addNotes)
+  .get('/notes/:id', Controller.getNoteById)
+  .put('/notes/:id', Controller.updateNotes)
+  .delete('/notes/:id', Controller.deleteNoteById);
+
+export default router;
